4997ef068fc3043d24865bbf8a78c95382912469
Ziru Zou <jiyucoding@yahoo.com>
2023-02-10 19:45:52 -0500

    render four NavLinks component with to props

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,15 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      {/*{code here}*/}
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="actors">Acotrs</NavLink>
+      <NavLink to="home">Home</NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
